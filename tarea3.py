45fda0ce9c61e57a31030140cba7829ed617da71
ospino02 <100149848+ospino02@users.noreply.github.com>
2022-02-21 11:04:38 -0500

    Add files via upload

---
+++ tarea3.py
@@ -0,0 +1,16 @@
+# hacer programa que compare 2 numeros y me diga sin son iguales 
+# o cual es el mayor 
+
+num_1= float(input("ingrese un numero: "))
+num_2= float(input("ingrese un numero: "))
+if num_1 == num_2:
+    print("son iguales")
+    
+elif num_1 >=num_2:
+    print("el mayor es", num_1)
+    
+    elif num_1 <= num_2:
+    print("el mayor es", num_2)
+
+
+
---
+++ tarea4.py
@@ -0,0 +1,2 @@
+while True:
+    print("estoy atrapado en el loop ")
